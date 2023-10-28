f9f6386c7ddc6aba1701572200bdf66305c28821
yang <yhe428@aucklanduni.ac.nz>
2023-10-28 19:27:04 +1300

    comment out

--- public/javascript/verify-password.js
+++ public/javascript/verify-password.js
@@ -1,20 +1,20 @@
 window.addEventListener("load", function() {
-    // let pathname = window.location.pathname;
+    let pathname = window.location.pathname;
 
-    // if(pathname == '/newAccount'){
-    //     const input = document.querySelector('#confirmPassword');
+    if(pathname == '/newAccount'){
+        const input = document.querySelector('#confirmPassword');
 
-    //     input.addEventListener("blur", function(){
-    //         const password1 = document.querySelector("#txtPassword").value;
-    //         const password2 = document.querySelector("#confirmPassword").value;
-    //         if(password1 == password2){
-    //             document.querySelector("#submit").disabled = false;
-    //         }else{
-    //             document.querySelector("#remind").innerHTML = "<span style ='color:red'>passwords do not match, please type again</span>";
-    //             document.querySelector("#submit").disabled = true;
-    //         }
+        input.addEventListener("blur", function(){
+            const password1 = document.querySelector("#txtPassword").value;
+            const password2 = document.querySelector("#confirmPassword").value;
+            if(password1 == password2){
+                document.querySelector("#submit").disabled = false;
+            }else{
+                document.querySelector("#remind").innerHTML = "<span style ='color:red'>passwords do not match, please type again</span>";
+                document.querySelector("#submit").disabled = true;
+            }
 
-    //     });
+        });
 
-    // }
+    }
 })
--- routes/newAccount.js
+++ routes/newAccount.js
@@ -60,18 +60,18 @@ router.post("/newAccount", async function (req, res) {
 
 });
 
-//router.get("/verifyUsername", async function (req, res) {
-//     let usernameToBeVerify = req.query.username;
+router.get("/verifyUsername", async function (req, res) {
+    let usernameToBeVerify = req.query.username;
 
-//     let returnedUser = await userDao.retrieveUserByName(usernameToBeVerify);
+    let returnedUser = await userDao.retrieveUserByName(usernameToBeVerify);
 
-//     if (returnedUser) {
-//         res.send(true);
-//     } else {
-//         res.send(false);
-//     }
+    if (returnedUser) {
+        res.send(true);
+    } else {
+        res.send(false);
+    }
 
-// })
+})
 
 router.get("/login", function (req, res) {
     if (res.locals.user) {
