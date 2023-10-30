b2461679dd8bf26e39bb73284a58433204982856
LB <lucy.birrell2@gmail.com>
2023-10-30 15:37:52 +1300

    route with id parameter

--- routes/photographer-routes.js
+++ routes/photographer-routes.js
@@ -7,13 +7,13 @@ router.get("/photographers", async function(req, res) {
 
     try{
         const users = await userDao.getAllUsers();
-        // console.log(users); return back a object array, not a object!
-        // res.locals.users = users;
+        res.locals.users = users;
 
         const firstPhotographer = await userDao.retrieveUserById(1);
-        // console.log(firstPhotographer); working, your previous code is getUserById, which is not exist in user-dao
         res.locals.photographer = firstPhotographer;
+
         res.render("photographers");
+
     }catch (error){
         res.status(500).send('Server error');
     }
@@ -22,22 +22,14 @@ router.get("/photographers", async function(req, res) {
 });
 
 router.get("/photographers/:userId", async function (req, res) {
-    let userId = req.params['userId'];
+    
+    let id = req.params['userId'];
+    const currentPhotographer = await userDao.retrieveUserById(id);
+  
+    res.locals.photographer = currentPhotographer;
 
-    try {
-        const currentPhotographer = await userDao.getUserById(userId);
-        console.log(currentPhotographer);
-
-            res.json(currentPhotographer);
-           
-        }
-     catch (error) {
-        console.error("An error occurred:", error);
-        res.sendStatus(500);
-
-    }
-
-     //res.render("photographers", { photographer: currentPhotographer });
+    res.render("photographers");
 });
+
 
 module.exports = router;
