7ecd876c24b77656b914e2fa9422b9484ad34b88
yang <yhe428@aucklanduni.ac.nz>
2023-11-04 14:55:39 +1300

    space changed

--- routes/application-routes.js
+++ routes/application-routes.js
@@ -5,64 +5,65 @@ const articlesDao = require("../modules/articles-dao.js");
 const commentDao = require("../modules/comment-dao.js");
 
 router.get("/", function (req, res) {
-    res.locals.title = "Dazzling Duck Photography"
-    res.render("home");
+  res.locals.title = "Dazzling Duck Photography";
+  res.render("home");
 });
 
 router.get("/blog", async function (req, res) {
-    res.locals.title = "Blog";
+  res.locals.title = "Blog";
 
-    //yang change 
-    const articles = await articlesDao.retrieveAllArticles();
-    res.locals.articles = articles;
+  //yang change
+  const articles = await articlesDao.retrieveAllArticles();
+  res.locals.articles = articles;
 
-    // const articles = await articlesDao.retrieveAllArticlesWithCommentCount();
-    // res.locals.articles = articles;
+  // const articles = await articlesDao.retrieveAllArticlesWithCommentCount();
+  // res.locals.articles = articles;
 
-
-    res.render("blog");
+  res.render("blog");
 });
 // router.get("/full-article/:articleId", async function(req, res)
 
-router.get("/full-article/:articleId", async function(req, res) {
-    let id = req.params['articleId'];
-    const article = await articlesDao.retrieveArticleByArticleId(id);
-    // console.log(article);
-    // res.locals.article = article;
-    
-    //yang's codes for comments
-    const comments = await commentDao.getNestedCommentsByArticleID(id);
-    const commentsCount = await commentDao.countCommentsByArticleID(id);
-    
+router.get("/full-article/:articleId", async function (req, res) {
+  let id = req.params["articleId"];
+  const article = await articlesDao.retrieveArticleByArticleId(id);
+  // console.log(article);
+  // res.locals.article = article;
 
-    res.render("full-article", { ...article, articleId: id, comments:comments, commentsCount:commentsCount});
+  //yang's codes for comments
+  const comments = await commentDao.getNestedCommentsByArticleID(id);
+  const commentsCount = await commentDao.countCommentsByArticleID(id);
 
+  res.render("full-article", {
+    ...article,
+    articleId: id,
+    comments: comments,
+    commentsCount: commentsCount,
+  });
 });
 
-
 router.get("/nature", async function (req, res) {
-    res.locals.title = "Nature";
+  res.locals.title = "Nature";
 
-    const natureArticles = await articlesDao.retrieveCategoryArticles("Nature");
+  const natureArticles = await articlesDao.retrieveCategoryArticles("Nature");
 
-    res.locals.articles = natureArticles;
-    res.render("nature");
+  res.locals.articles = natureArticles;
+  res.render("nature");
 });
 
 router.get("/portrait", async function (req, res) {
-    res.locals.title = "Portrait";
-    const portraitArticles = await articlesDao.retrieveCategoryArticles("Portrait");
-    res.locals.articles = portraitArticles;
-    res.render("portrait");
+  res.locals.title = "Portrait";
+  const portraitArticles = await articlesDao.retrieveCategoryArticles(
+    "Portrait"
+  );
+  res.locals.articles = portraitArticles;
+  res.render("portrait");
 });
 
 router.get("/life", async function (req, res) {
-    res.locals.title = "Life";
-    const lifeArticles = await articlesDao.retrieveCategoryArticles("Life");
-    res.locals.articles = lifeArticles;
-    res.render("life");
+  res.locals.title = "Life";
+  const lifeArticles = await articlesDao.retrieveCategoryArticles("Life");
+  res.locals.articles = lifeArticles;
+  res.render("life");
 });
-
-
 
 module.exports = router;
